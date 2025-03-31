287d250829dd5eebf1729840fd213c772985d208
Dimitar Nikolov <90249344+diela33@users.noreply.github.com>
2025-04-01 01:04:08 +0300

    delete WelcomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import WelcomePage from './components/WelcomePage';
+
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<WelcomePage />} />
-                <Route path="/login" element={<div>Login Page</div>} />
-                <Route path="/register" element={<div>Register Page</div>} />
-                <Route path="/recipes" element={<div>Recipes Page</div>} />
-            </Routes>
-        </Router>
+        <h2>
+            klk
+        </h2>
+
     );
 }
 
